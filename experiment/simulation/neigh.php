314532cc382d027da5029da1c1be7ee48e702c3e
sravanthimodepu <sravanthimodepu@gmail.com>
2021-10-29 17:04:42 +0530

    fixed simulation issue

--- experiment/simulation/neigh.php
+++ experiment/simulation/neigh.php
@@ -31,7 +31,7 @@ if(isset($_GET["assess"])) {
 <!-- This file was originally generated at http://psd2cssonline.com on September 16, 2010, 10:04 am -->
 <!-- psd2css Online version 1.85 -->
 
-<!--<title>Virtual Lab in Image Processing</title>-->
+<title>Virtual Lab in Image Processing</title>
 
 
 <!-- Some META tags to help with Search Engine Optimization.  Please 
@@ -523,22 +523,22 @@ $("#sliderContent").replaceWith('      <div id="sliderContent" class="ui-corner-
 
     <!-- This is 'TopBar_jpg' -->
     <div id="Layer-3" class="TopBar_jpg"  >
-      <img src="images/Layer-3.jpg" width="894" height="96" alt="TopBar" />
+      <!-- <img src="images/Layer-3.jpg" width="894" height="96" alt="TopBar" /> -->
       <!-- This is 'IIIT' -->
-<!--      <div id="Layer-6" class="IIIT"  >
+      <!-- <div id="Layer-6" class="IIIT"  >
         <img src="images/iiit.png" width="100" height="70" alt="IIIT" class="pngimg" />
-	</div>-->
+	</div> -->
 	
-		<!--	<div id="topMenu">
-			<h1 style="text-align: center;">Virtual Lab for IMAGE PROCESSING</h1>
-<div class="home">
+			<div id="topMenu">
+			<!-- <h1 style="text-align: center;">Virtual Lab for IMAGE PROCESSING</h1> -->
+<!-- <div class="home">
 <a onclick="confirmTo('index.html')">home</a>
-</div>-->
+</div> -->
 
-		<!--	<div class="menu">
+			<div class="menu">
 
 
-<ul>
+<!-- <ul>
 <li><a href="objective.php?exp=neigh" target="_self" >Objective</a>
 
 
@@ -565,17 +565,17 @@ $("#sliderContent").replaceWith('      <div id="sliderContent" class="ui-corner-
 					<li><a href="assign.php?exp=neigh">Assignment</a></li>
 			   </ul>
 
-</li>-->
+</li> -->
 <!--
 <li><a href="references.php?exp=neigh" target="_self" >References</a>
 </li>
 -->
 
-<!--<li><a href="summary.php" target="_blank" >Summary</a>
+<!-- <li><a href="summary.php" target="_blank" >Summary</a>
 
 </li>
-</ul>
-</div>-->
+</ul> -->
+</div>
 			</div>
 			
     </div>
